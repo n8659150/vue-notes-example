4db4ebd99b4385b9afdb8a9265e570add5c51a85
wanghao8080 <117663444@qq.com>
2017-10-15 14:04:45 +0800

    创建笔记应用

---
+++ app.js
@@ -0,0 +1,29 @@
+const Note = {
+  template: `
+    <div>笔记</div>
+  `
+}
+
+const Notes = {
+  components: {
+    'note': Note
+  },
+  template: `
+    <div>
+      <a>添加笔记</a>
+      <note></note>
+      <note></note>
+      <note></note>
+    </div>
+  `
+}
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    'notes': Notes
+  },
+  template: `
+    <notes></notes>
+  `
+})
