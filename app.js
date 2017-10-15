ee356a109d6ff45bf64cc440a7ffd26182717140
wanghao8080 <117663444@qq.com>
2017-10-15 16:19:54 +0800

    显示笔记列表

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const Note = {
+  props: [
+    'entityObject'
+  ],
+  data() {
+    return {
+      entity: this.entityObject
+    }
+  },
   template: `
     <div class="item">
       <div class="content">
-        <div class="header">笔记</div>
+        <div class="header">
+          {{ entity.body }}
+        </div>
       </div>
     </div>
   `
@@ -36,9 +46,11 @@ const Notes = {
       </h4>
       <a class="ui right floated basic violet button">添加笔记</a>
       <div class="ui divided items">
-        <note></note>
-        <note></note>
-        <note></note>
+        <note
+          v-for="entity in entities"
+          v-bind:entityObject="entity"
+          v-bind:key="entity.$loki">
+        </note>
       </div>
     </div>
   `
